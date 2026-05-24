37b561edb370a2bacbc81758ec6b76d5ca145edc
Ellie Kweon <kweondew@gmail.com>
2026-05-23 17:52:48 -0700

    explain basic code

---
+++ text.rb
@@ -0,0 +1,29 @@
+puts "chiikawa" + "hamster"
+puts "chiikawa" * 4
+# puts "chiikawa" * "4"
+
+puts ""
+puts 12 + 12
+puts "12" + "12"
+puts "12 + 12"
+
+puts ""
+puts "How many months old is Leika"
+puts "Leiks is #{ 12 * 4.7 } months"
+puts 'Leiks is #{ 12 * 4.7 } months'
+# what is between "" + ''
+
+puts "\t anyword \n another word"
+puts '\t anyword \n another word'
+
+letters = "diNeRo"
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts 'e'.capitalize
+puts ' e'.capitalize
+puts letters
+# methods
+puts(letters)
+# functions
