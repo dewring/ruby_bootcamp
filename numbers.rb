c2f06d065bdaeeb91eaf38ff5772824942313aec
Ellie Kweon <kweondew@gmail.com>
2026-05-23 17:33:40 -0700

    ruby numbers excercise

---
+++ numbers.rb
@@ -0,0 +1,37 @@
+puts "First Excercise"
+puts 1 + 2
+puts 3
+puts 3.0
+puts "Ellie"
+
+2 + 7
+
+puts ""
+puts "Second Exercise"
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts ""
+puts "Third Exercise"
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2 # 4
+puts "9 = (2 + 2 + 2 + 2) + 1"
+puts 9 % 2
+
+puts 20 / 3 
+puts "(3 + 3 + 3 + 3 + 3 + 3) + 2"
+puts 20 % 3
+
+puts ""
+puts "How many months old is Leika"
+puts "Leiks is #{ 12 * 4.7 } months"
+
+
+leika_months_number = 12 * 4.7
+rounded_leika_months_number = leika_months_number.round(3)
+puts "Leiks is #{rounded_leika_months_number} months"
+
