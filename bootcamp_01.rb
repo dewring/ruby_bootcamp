e666ff40e4f094386142eca9da375fb101c0f8a7
Ellie Kweon <kweondew@gmail.com>
2026-05-24 14:55:23 -0700

    chagne unique cry add customerror

--- bootcamp_01.rb
+++ bootcamp_01.rb
@@ -1,17 +1,20 @@
 class Animal
-  def initialize(name, color)
-    @name = name
-    @color = color
+  class UndefindAnimalError < StandardError
   end
-  def info
-    {
-      name: @name,
-      color: @color,
-    }
-  end
-  def cry
-    "#{@name} is crying"
-  end
+  
+    def initialize(name, color)
+      @name = name
+      @color = color
+    end
+    def info
+      {
+        name: @name,
+        color: @color,
+      }
+    end
+    def cry
+        raise UndefindAnimalError, "You put undefined animal. It can't cry."
+    end
 end
 
 class Hamster < Animal
@@ -19,6 +22,9 @@ class Hamster < Animal
     animal_info = super
     animal_info[:type] = "Hamster"
     animal_info
+  end
+  def cry
+    "와앙"
   end
 end
 
@@ -28,12 +34,19 @@ class Dog < Animal
     animal_info[:type] = "Dog"
     animal_info
   end
+  def cry
+    "멍멍"
+  end
 end
 
 hamster = Hamster.new("Chiikawa", "White")
 puts hamster.info
-puts hamster.cry
+puts "#{hamster.info[:type]} cry like #{hamster.cry}" 
 
 dog = Dog.new("Leika", "Gold")
 puts dog.info
-puts dog.cry
+puts "#{dog.info[:type]} cry like #{dog.cry}" 
+
+animal = Animal.new("Cat", "Blue")
+puts animal.info
+puts "#{animal.info[:type]} cry like #{animal.cry}" 
